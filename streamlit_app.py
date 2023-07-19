a34781684beb301e2b398614f8e3dac9ee1e7a1f
CloudLearner071923 <140013714+CloudLearner071923@users.noreply.github.com>
2023-07-19 15:45:17 -0500

    Update streamlit_app.py
    trying to fix the index to pick fruit name instead of number

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #We'll add a user interactive widget called a Multi-select that will allow users to pick the fruits they want in their smoothies.
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
