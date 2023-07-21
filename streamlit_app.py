1f4218f8e2683eb4728cb53139e072fd1d19fb87
CloudLearner071923 <140013714+CloudLearner071923@users.noreply.github.com>
2023-07-21 15:11:37 -0500

    Update streamlit_app.py
    Now Let's Use a Function and Button to Add the Fruit Name Submissions

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,16 +115,30 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
+##
+## do not run anything past here while we troubleshoot
+#streamlit.stop()
+
+#Now Let's Use a Function and Button to Add the Fruit Name Submissions
+
+##ask user for input to add fruit to the list, no default
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+##streamlit.write('The user entered ', add_my_fruit)
+#streamlit.write('Thanks for adding ', add_my_fruit)
 #
-# do not run anything past here while we troubleshoot
-streamlit.stop()
+##this should fail, but go with it for now
+##control flow problem, meaning this will execute each time we interact with the page
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
-#ask user for input to add fruit to the list, no default
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+         return "Thanks for adding " + new_fruit
+
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-#streamlit.write('The user entered ', add_my_fruit)
-streamlit.write('Thanks for adding ', add_my_fruit)
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
 
-#this should fail, but go with it for now
-#control flow problem, meaning this will execute each time we interact with the page
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
+#The only problem is, we're still not adding the fruit name. We still have "from streamlit" hard-coded in. 
