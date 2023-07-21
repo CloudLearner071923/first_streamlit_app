df6642515f5ac85e5eaf1e3c6b0ffa6f839d8047
CloudLearner071923 <140013714+CloudLearner071923@users.noreply.github.com>
2023-07-21 09:32:32 -0500

    Update streamlit_app.py
    added comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#http response
 #streamlit.text(fruityvice_response)
+#json output
 streamlit.text(fruityvice_response.json())
 
 
