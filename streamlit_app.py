cbb4d1f343a6ceaa46ee7ae8d7e73d30ac75c5e7
CloudLearner071923 <140013714+CloudLearner071923@users.noreply.github.com>
2023-07-21 09:59:24 -0500

    Update streamlit_app.py
    ask user for input, default to Kiwi fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,10 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
+#ask user for input, default to Kiwi fruit
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #change to use variable
