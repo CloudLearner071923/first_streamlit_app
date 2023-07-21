1aae7c405a8ba4fc470ef7e5cb286e5edbd40707
CloudLearner071923 <140013714+CloudLearner071923@users.noreply.github.com>
2023-07-21 14:39:02 -0500

    Update streamlit_app.py
    added repeatable code block, a function, to retrieve the fruit choice and pass that back for display

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,6 +61,12 @@ streamlit.dataframe(fruits_to_show)
 ##output it to the screen
 #streamlit.dataframe(fruityvice_normalized)
 
+#create a function - repeatable code block
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+   
 #New section to display fruityvice api response using try-except (with nested if-else)
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -68,9 +74,11 @@ try:
    if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
    else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+#        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#        streamlit.dataframe(fruityvice_normalized)
+         back_from_function = get_fruityvice_data(fruit_choice)
+         streamlit.dataframe(back_from_function)
 
 except URLError as e:
     streamlit.error()
